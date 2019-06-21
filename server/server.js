f3e855808227b93bde59acd7281c1b2e073faeea
kiqecanijo <enry1375@gmail.com>
2019-06-20 23:45:28 -0500

    temporal fix for node server request

--- server/server.js
+++ server/server.js
@@ -1,8 +1,31 @@
 import express from 'express'
+import formidable from 'express-formidable'
+import request from 'request'
+
 const app = express()
-const port = 3001
+app.use(formidable())
 
-app.listen(port,()  =>
-  console.log(`Server is listening on port ${port}`))
+const port = 3000
 
-module.exports = app;
+const options = {
+  method: 'GET',
+  url: 'https://api.github.com/search/topics',
+  qs: { q: 'sample' },
+  headers: {
+    Accept: 'application/vnd.github.mercy-preview+json',
+    'User-Agent': 'list-topics-app'
+  }
+}
+
+app.listen(port, () => console.log(`´:{ Done !! running server on: ${port}`))
+
+app.post('/', (req, res) => {
+  req.fields.q
+    ? request(options, function(error, response, body) {
+        if (error) throw new Error(error)
+        res.status(200).send(body)
+      })
+    : res.status(303).send('Magic word is required :(')
+})
+
+module.exports = app
